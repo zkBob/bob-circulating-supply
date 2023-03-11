b3a34dcdb37ddcc25c5a0cb3d234abf0ebb844c3
Alexander Kolotov <alexandr.kolotov@gmail.com>
2023-03-11 22:25:28 +0400

    attempt to solve HTTPS redirects issue

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 import uvicorn
 from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
-
+from fastapi.responses import PlainTextResponse, RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 
-from utils.logging import LoggerProvider
-
 from bobstats.router import router as stats_router
+from bobstats.web import BobStats
+from bobstats.models import BobStatsDataForTwoPeriods
+
 from supply.router import router as supply_router
+from supply.web import TotalSupply
+
 from bobvault.router import router as vault_router
+
+from utils.logging import LoggerProvider
 from utils.settings import Settings
 from utils.health import HealthRegistry, HealthOut
 
@@ -20,13 +24,27 @@ app.add_middleware(
     allow_origins=["*"]
 )
 
-app.include_router(stats_router, prefix="/bobstat")
+app.include_router(stats_router, prefix="/bobstats")
 app.include_router(vault_router, prefix="/coingecko/bobvault")
 app.include_router(supply_router, prefix="/supply")
 
 @app.get("/", response_class=RedirectResponse)
 async def root() -> str:
-    return '/supply'
+    return '/supply/'
+
+@app.get("/bobstat", response_class=RedirectResponse)
+async def legacy_bobstat() -> str:
+    return '/bobstats/'
+
+# # Continue use legacy approach otherwise redirect return HTTP rather than HTTPS url
+# @app.get("/", response_class=PlainTextResponse)
+# async def root() -> str:
+#     return str(TotalSupply().value)
+
+# # Continue use legacy approach otherwise redirect return HTTP rather than HTTPS url
+# @app.get("/bobstat", response_model=BobStatsDataForTwoPeriods)
+# async def provide() -> BobStatsDataForTwoPeriods:
+#     return BobStats().load()
 
 @app.get("/health", response_model=HealthOut, response_model_exclude_none=True)
 async def health():
