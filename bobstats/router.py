b4fba7620a6bb85a9e3e9410e1d90f5d274fc765
Alexander Kolotov <alexandr.kolotov@gmail.com>
2023-05-30 00:21:58 +0300

    Add ability to get BOB Protocol yield (#4)

--- bobstats/router.py
+++ bobstats/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Security
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
 from .web import BobStats
-from .models import BobStatsDataForTwoPeriods
+from .models import BobStatsDataForTwoPeriodsAPI, BobStatsDataForTwoPeriodsToFeed, GainStatsAPI
 
 from utils.misc import check_auth_token
 from utils.models import UploadResponse
@@ -11,12 +11,16 @@ _security = HTTPBearer()
 
 router = APIRouter()
 
-@router.get("/", response_model=BobStatsDataForTwoPeriods)
-async def provide() -> BobStatsDataForTwoPeriods:
-    return BobStats().load()
+@router.get("/", response_model=BobStatsDataForTwoPeriodsAPI)
+async def provide() -> BobStatsDataForTwoPeriodsAPI:
+    return BobStats().loadMainStat()
+
+@router.get("/yield", response_model=GainStatsAPI, response_model_exclude_unset=True)
+async def provide() -> GainStatsAPI:
+    return BobStats().loadYieldStat()
 
 @router.post("/upload", response_model=UploadResponse)
-async def upload(data: BobStatsDataForTwoPeriods, \
+async def upload(data: BobStatsDataForTwoPeriodsToFeed, \
                  credentials: HTTPAuthorizationCredentials = Security(_security)) -> UploadResponse:
     if not check_auth_token(credentials.credentials):
         return UploadResponse(status="Incorrect auth token")
