df9509ac7af4fbf92f06929da905f30c9c9d6f8d
Alexander Kolotov <alexandr.kolotov@gmail.com>
2023-05-29 15:18:22 -0600

    Add ability to get BOB Protocol yield

--- bobstats/models.py
+++ bobstats/models.py
@@ -1,16 +1,82 @@
 from decimal import Decimal
+from typing import Optional, List
 
-from pydantic import Extra
+from pydantic import Extra, BaseModel, Field
 
 from utils.models import TimestampedBaseModel
 
-class BobStatsPeriodData(TimestampedBaseModel, extra=Extra.forbid):
+class OneTokenAcc(BaseModel):
+    symbol: str
+    amount: Decimal
+
+GainSet = List[OneTokenAcc]
+
+class GainStats(BaseModel):
+    fees: GainSet
+    interest: Optional[GainSet]
+
+    def is_empty(self) -> bool:
+        retval = True
+        if not self.is_fees_empty():
+            retval = False
+        if not self.is_interest_empty():
+            retval = False
+        return retval
+    
+    def is_fees_empty(self) -> bool:
+        retval = True
+        if self.fees and len(self.fees) > 0:
+            retval = False
+        return retval
+
+    def is_interest_empty(self) -> bool:
+        retval = True
+        if self.interest and len(self.interest) > 0:
+            retval = False
+        return retval
+
+    def adjust(self, source):
+        def adjust_gain_set(fees_source: GainSet, fees_target: GainSet):
+            for f in fees_source:
+                found = False
+                for target in fees_target:
+                    if target.symbol == f.symbol:
+                        found = True
+                        target.amount += f.amount
+                        break
+                if not found:
+                    fees_target.append(f)
+            
+        if not source.is_fees_empty():
+            if not self.fees:
+                self.fees = []
+            adjust_gain_set(source.fees, self.fees)
+        if not source.is_interest_empty():
+            if not self.interest:
+                self.interest = []
+            adjust_gain_set(source.interest, self.interest)
+
+class GainStatsTimeStamped(GainStats, extra=Extra.forbid):
+    timestamp: int
+
+class GainStatsAPI(TimestampedBaseModel, extra=Extra.forbid):
+    timestamp: int
+    gain: GainStatsTimeStamped = Field(..., alias='yield') 
+
+class BobStatsPeriodDataAPI(TimestampedBaseModel):
     totalSupply: Decimal
     collaterisedCirculatedSupply: Decimal
     volumeUSD: Decimal
     holders: int
 
-class BobStatsDataForTwoPeriods(TimestampedBaseModel, extra=Extra.forbid):
-    current: BobStatsPeriodData
-    previous: BobStatsPeriodData
+class BobStatsPeriodDataToFeed(BobStatsPeriodDataAPI, extra=Extra.forbid):
+    gain: Optional[GainStats]
+
+class BobStatsDataForTwoPeriodsAPI(TimestampedBaseModel, extra=Extra.forbid):
+    current: BobStatsPeriodDataAPI
+    previous: BobStatsPeriodDataAPI
+
+class BobStatsDataForTwoPeriodsToFeed(TimestampedBaseModel, extra=Extra.forbid):
+    current: BobStatsPeriodDataToFeed
+    previous: BobStatsPeriodDataToFeed
 
--- bobstats/web.py
+++ bobstats/web.py
@@ -1,6 +1,6 @@
 from functools import cache
 from time import time
-from json import dump
+from json import dump, load
 from decimal import Decimal
 
 from pydantic import ValidationError
@@ -24,51 +24,90 @@ class BobStats(Health):
         self.initialize_healthdata()
         HealthRegistry().append(self)
 
-    def _dump(self, data: BobStatsDataForTwoPeriods):
+    def _dump(self, data: BobStatsDataForTwoPeriodsToFeed):
         with open(self.filename, 'w') as json_file:
-            dump(data.dict(), json_file, cls=CustomJSONEncoder)
+            dump(data.dict(exclude_unset=True), json_file, cls=CustomJSONEncoder)
 
-    def _load(self) -> BobStatsDataForTwoPeriods:
+    def _load_json_as_dict(self) -> dict:
         try:
             with open(self.filename, 'r') as json_file:
-                data = ''.join(json_file.readlines())
-                data = BobStatsDataForTwoPeriods.parse_raw(data)
+                data = load(json_file)
             return data
         except IOError as e:
             warning(f'No snapshot {self.filename} found')
             raise e
+
+    def _loadMainStat(self) -> BobStatsDataForTwoPeriodsAPI:
+        data = self._load_json_as_dict()
+        try:
+            return BobStatsDataForTwoPeriodsAPI.parse_obj(data)
         except ValidationError as e:
             error(f'Cannot parse snapshot data')
             raise e
 
-    def store(self, data: BobStatsDataForTwoPeriods):
+    def _loadYieldStat(self) -> GainStatsTimeStamped:
+        data = self._load_json_as_dict()
+        info(f"{type(data)}")
+        try:
+            out = data['current']['gain']
+            out['timestamp'] = data['current']['timestamp']
+            return GainStatsTimeStamped.parse_obj(out)
+        except ValidationError as e:
+            error(f'Cannot parse snapshot data')
+            raise e
+
+    def _load(self) -> BobStatsDataForTwoPeriodsAPI:
+        return self._loadMainStat()
+
+    def store(self, data: BobStatsDataForTwoPeriodsToFeed):
         info(f'New bobstat data stamped as {data.timestamp} received')
 
         self._dump(data)
         
         self.record_sucess(data.timestamp)
 
-    def load(self):
-        ts_checkpoint = time()
+    def loadMainStat(self) -> BobStatsDataForTwoPeriodsAPI:
+        ts_checkpoint = int(time())
 
         zero = Decimal(0)
 
-        empty_period = BobStatsPeriodData(
+        empty_period = BobStatsPeriodDataAPI(
             timestamp=0,
             totalSupply=zero,
             collaterisedCirculatedSupply=zero,
             volumeUSD=zero,
             holders=0
         )
-        empty_response = BobStatsDataForTwoPeriods(
-            timestamp=int(ts_checkpoint),
+        empty_response = BobStatsDataForTwoPeriodsAPI(
+            timestamp=ts_checkpoint,
             current=empty_period,
             previous=empty_period
         )
 
         try:
-            data = self._load()
-            data.timestamp=int(ts_checkpoint)
+            data = self._loadMainStat()
+            data.timestamp=ts_checkpoint
+        except:
+            return empty_response
+
+        return data
+
+    def loadYieldStat(self) -> GainStatsAPI:
+        ts_checkpoint = int(time())
+
+        empty_response = GainStatsAPI(**{
+            'timestamp': ts_checkpoint,
+            'yield': GainStatsTimeStamped(
+                timestamp = 0,
+                fees = []
+            )
+        })
+
+        try:
+            data = GainStatsAPI(**{
+                'timestamp': ts_checkpoint,
+                'yield': self._loadYieldStat()
+            })
         except:
             return empty_response
 
